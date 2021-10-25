12610e7f386a2d33033c47a3cbaabff2953d12d3
koddi9 <mors.1.course@gmail.com>
2021-10-26 00:47:59 +0300

    Commit1

--- src/main/java/AppConfig.java
+++ src/main/java/AppConfig.java
@@ -4,11 +4,20 @@ import org.springframework.context.annotation.Scope;
 
 @Configuration
 public class AppConfig {
- 
     @Bean(name="helloworld")
+    @Scope("singleton")
     public HelloWorld getHelloWorld() {
         HelloWorld helloWorld = new HelloWorld();
         helloWorld.setMessage("Hello World!");
         return helloWorld;
     }
+
+    @Bean(name="cat")
+    @Scope("prototype")
+    public Cat getCatAct(){
+        Cat cat = new Cat();
+        cat.setCatAct("Meow");
+        return cat;
+    }
+
 }
---
+++ src/main/java/Cat.java
@@ -0,0 +1,14 @@
+public class Cat {
+    private String catAct;
+    public Cat(){
+
+    }
+
+    public String getCatAct() {
+        return catAct;
+    }
+
+    public void setCatAct(String catAct) {
+        this.catAct = catAct;
+    }
+}
